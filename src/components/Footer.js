089ba90f09f8abf82f6d1321dc7dc1d46b723226
Isabela Cunha <99668314+isabelalimadacunha@users.noreply.github.com>
2024-06-23 16:00:13 -0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import '../index.css';
+
+function Footer(){
+
+    return(
+    <div className='footer-box'>
+       <h1>Teste</h1>    
+    </div>
+    );
+    
+    } 
+    export default Footer;
